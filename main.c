0f70c0424fec6b36a37a08b2a4c61c36025736e8
Joseph <cutinoj@mail.gvsu.edu>
2018-02-12 09:42:40 -0500

    comments

--- main.c
+++ main.c
@@ -53,9 +53,7 @@ int main(int argc, char *argv[]){
 		fgets(collection_filename, sizeof(collection_filename),stdin);
 		
 		//Remove trailing '\n'
-		if((collection_filename[strlen(collection_filename)-1]='\n')){
-			collection_filename[strlen(collection_filename)-1]='\0';
-		}
+		collection_filename[strlen(collection_filename)-1]='\0';
 		
 		//Attempt to open collection
 		collection = fopen(collection_filename,"r");
